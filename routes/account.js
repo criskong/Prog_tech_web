ac1b0b949c34c49f96ac49d6a2f7ccd5ceb1fedf
lecodyme <giovanni.belluomo@outlook.it>
2022-12-07 17:28:59 +0100

    Added obtainment of the orders
    Added the obtainment of the list of the orders associated to an account

--- routes/account.js
+++ routes/account.js
@@ -2,21 +2,22 @@ var express = require('express');
 const passport = require('passport');
 var router = express.Router();
 var Account = require('../models/Account');
+var Order = require('../models/Order');
 
 /* GET Account page. */
 router.get('/', async (req, res, next) => {
   
   if(req.isAuthenticated())
   {
-    //Getting the info of the account related to the session
-    var aqres = await Account.findById(req.session.passport.user);
-
-    //TODO: getting all the orders associated with the account
 
     //Rendering the page with the required parameters
-    res.render('logged_in/account', { 
-      account: aqres,
-      orders:[{}]
+    res.render('logged_in/account', {
+      
+      //Info of the account related to the session
+      account: await Account.findById(req.session.passport.user),
+
+      //TODO: Orders associated with the account
+      orders: await Order.find({ owner_id: req.session.passport.user })
     });
 
   }
