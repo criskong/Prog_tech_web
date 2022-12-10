413cce4a6d54b3638a97d2a6fbbdd0c7061a92a5
lecodyme <giovanni.belluomo@outlook.it>
2022-12-10 20:37:34 +0100

    Added data handling and visualization to savedmenu
    Added data handling and visualization to the savedmenu page

--- routes/savedmenu.js
+++ routes/savedmenu.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var SavedMenu = require('../models/SavedMenu');
+const ejs = require('ejs');
 
 /* GET saved menus page. */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res, next) => {
 
   if(req.isAuthenticated())
-    res.render('logged_in/savedmenu');
+  {
+    res.send(await ejs.renderFile('./views/logged_in/savedmenu.ejs',{
+
+      //Saved menus related to the account
+      savedmenus: await SavedMenu.find({ owner_id: req.session.passport.user }),
+
+    },{async: true}));
+  }
   else
     res.redirect('/login?failed=false');
 });
