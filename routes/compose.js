7c6a09c7e377061fb79db4e36103ba1f4b4fd501
lecodyme <giovanni.belluomo@outlook.it>
2022-12-10 19:06:06 +0100

    Added data handling and visualization
    Added data handling and visualization for the compose page

--- routes/compose.js
+++ routes/compose.js
@@ -1,11 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var Item = require('../models/Item');
 
-/* GET Page for composing a menu. */
-router.get('/', function(req, res, next) {
+/* GET Page for composing a menu: seller choosing */
+router.get('/', async (req, res, next) => {
 
   if(req.isAuthenticated())
-    res.render('logged_in/compose');
+    res.render('logged_in/compose',{
+      //Avaliable sellers
+      sellers: await Item.distinct('seller')
+    });
+  else
+    res.redirect('/login?failed=false');
+});
+
+/* GET Page for composing a menu: items choosing */
+router.get('/seller/:seller', async (req, res, next) => {
+
+  if(req.isAuthenticated())
+    res.render('logged_in/compose',{
+
+      //Avaliable items labeled as 'primo'
+      primi: await Item.find({ 
+        seller: req.params.seller, 
+        type: 'primo'
+      }),
+
+      //Avaliable items labeled as 'secondo'
+      secondi: await Item.find({ 
+        seller: req.params.seller, 
+        type: 'secondo'
+      }),
+
+      //Avaliable items labeled as 'contorno'
+      contorni: await Item.find({ 
+        seller: req.params.seller, 
+        type: 'contorno'
+      }),
+
+      //Avaliable items labeled as 'bibita'
+      bibite: await Item.find({ 
+        seller: req.params.seller,
+        type: 'bibita'
+      })
+
+    });
   else
     res.redirect('/login?failed=false');
 });
