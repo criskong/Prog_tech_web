7728a039aad70acf152f62dae47700c3607f9910
lecodyme <giovanni.belluomo@outlook.it>
2022-12-11 01:53:01 +0100

    Implemented data handling and visualization for the order details page
    Implemented data handling and visualization for the order details page. Also added the possibility to show the QR code, and other buttons for handling a payment or elimination in case of a non payed order

--- routes/order.js
+++ routes/order.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var Order = require('../models/Order');
+var QR = require('qrcode');
 
 /* GET Order details page */
-router.get('/:orderId', async (req, res, next) => {
+router.get('/:orderId([0-9a-fA-F]{24})', async (req, res, next) => {
     
+    //Getting order info
+    var order = await Order.findById(req.params.orderId);
+
     if(req.isAuthenticated())
     {
         //Rendering the page with the required parameters
         res.render('logged_in/order', {
             //Info of the requested order
-            order: await Order.findById(req.params.orderId)
+            order: order,
+
+            //Generated QR code
+            qr: await QR.toDataURL(req.params.orderId,{width: 256})
+
         });
     }
     else
