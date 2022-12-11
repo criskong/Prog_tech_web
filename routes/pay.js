91969b3949ecd9b9f284ac8b6dea1cf699f25d5e
lecodyme <giovanni.belluomo@outlook.it>
2022-12-11 11:20:52 +0100

    Implemented and handled the payment of an existing order
    Implemented and handled the payment of an existing order that was not previously paid

--- routes/pay.js
+++ routes/pay.js
@@ -88,6 +88,63 @@ router.post('/', async (req, res, next) => {
 
 });
 
+/* GET Pay page for an already created order */
+router.get('/:order([0-9a-fA-F]{24})', async (req, res, next) => {
+
+  /*
+    Checking if the user is authenticated and
+     acting properly if not
+  */
+  if(!req.isAuthenticated())
+    res.redirect('/login?failed=false');
+
+  var order = await Order.findById(req.params.order);
+
+  //Creating paypal payment request data
+  var payment_json = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/pay/success/"+order._id,
+        "cancel_url": "http://localhost:3000/account"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "Ordine Enjoyfood",
+                "sku": order._id,
+                "price": order.total_price,
+                "currency": "EUR",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "EUR",
+            "total": order.total_price
+        },
+        "description": "Ordine EnjoyFood per "+order.target_seller
+    }]
+  };
+
+  paypal.payment.create(payment_json, function (error, payment) {
+
+    if (error)
+        res.redirect('/account');
+    else 
+    {
+      for(let i = 0;i < payment.links.length;i++)
+      {
+        if(payment.links[i].rel === 'approval_url')
+          res.redirect(payment.links[i].href);
+      }
+    }
+
+  });
+
+});
+
 /* GET Success payment page. */
 router.get('/success/:orderId', async (req, res, next) => {
 
